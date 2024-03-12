e90e3f452a4263b71490016bdffe2ac357f1de9b
안동민 <andongmin94@gmail.com>
2024-03-12 10:30:20 +0900

    Fix : chang icon directory, main.js blanks

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import https from "https";
 import dotenv from "dotenv";
-import { join, dirname } from "path";
 import { fileURLToPath } from "url";
+import { join, dirname } from "path";
 import electronLocalshortcut from "electron-localshortcut";
 import { app, ipcMain, BrowserWindow, globalShortcut, Tray, Menu, nativeImage } from "electron";
 
@@ -14,46 +14,46 @@ let tray;
 function createWindow() {
   // 브라우저 창을 생성합니다.
   mainWindow = new BrowserWindow({
-    width: 1700,
+    width: 1600,
     height: 900,
     frame: false,
 
-    icon: join(dirname(fileURLToPath(import.meta.url)), "icon.png"),
-    webPreferences: {preload: join(dirname(fileURLToPath(import.meta.url)), "preload.js")},
+    icon: join(dirname(fileURLToPath(import.meta.url)), "../../public/icon.png"),
+    webPreferences: { preload: join(dirname(fileURLToPath(import.meta.url)), "preload.js") },
   });
 
   mainWindow.loadURL(BASE_URL);
 
-  electronLocalshortcut.register(mainWindow, "F12", () => {console.log("F12 is pressed"); mainWindow.webContents.toggleDevTools()});
+  electronLocalshortcut.register(mainWindow, "F12", () => { console.log("F12 is pressed"); mainWindow.webContents.toggleDevTools() });
 
-  ipcMain.on("minimize", (event) => {mainWindow.minimize()});
+  ipcMain.on("minimize", (event) => { mainWindow.minimize() });
 
   ipcMain.on("maximize", (event) => {
     if (mainWindow.isMaximized()) {
-        mainWindow.restore();
-        console.log("restoring")}
-    else {
-      mainWindow.maximize(); console.log("maximizing")
-    }});
+      mainWindow.restore();
+      console.log("restoring");
+    } else {
+      mainWindow.maximize();
+      console.log("maximizing");
+    }
+  });
 
-  ipcMain.on("hidden", (event) => {mainWindow.hide()});
+  ipcMain.on("hidden", (event) => { mainWindow.hide() });
 
-  app.on("window-all-closed", () => {if (process.platform !== "darwin") {app.quit()}});
+  app.on("window-all-closed", () => { if (process.platform !== "darwin") { app.quit() } });
 
-  app.on("activate", () => {if (BrowserWindow.getAllWindows().length === 0) {createWindow()}});
+  app.on("activate", () => { if (BrowserWindow.getAllWindows().length === 0) { createWindow() } });
 }
 
 // 이 메소드는 Electron의 초기화가 완료되고
 // 브라우저 윈도우가 생성될 준비가 되었을때 호출된다.
 app.whenReady().then(createWindow).then(async () => {
-    const iconPath = join(dirname(fileURLToPath(import.meta.url)), "icon.png");
-    const icon = nativeImage.createFromPath(iconPath);
+    const icon = nativeImage.createFromPath(join(dirname(fileURLToPath(import.meta.url)), "../../public/icon.png"));
 
     globalShortcut.register('F5', () => {
       let win = BrowserWindow.getFocusedWindow();
       if (win) {
         win.reload();
-        connect();
       }
     });
 
@@ -66,7 +66,7 @@ app.whenReady().then(createWindow).then(async () => {
 
     tray.setToolTip("react-electron-boilerplate");
     tray.setContextMenu(contextMenu);
-    tray.on("double-click", () => {mainWindow.show()});
+    tray.on("double-click", () => { mainWindow.show() });
 
     app.on('will-quit', () => {
       // 애플리케이션이 종료되기 전에 모든 단축키를 해제합니다.
