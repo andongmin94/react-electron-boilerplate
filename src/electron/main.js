1524a53341f0e2af1f36c338bb640d901c21d1d7
김상훈 <k1016h@naver.com>
2024-03-12 21:48:00 +0900

    Feat : enhance code visible

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,17 +1,18 @@
-import axios from "axios";
-import https from "https";
-import dotenv from "dotenv";
+// 일렉트론 모듈
 import { fileURLToPath } from "url";
 import { join, dirname } from "path";
 import electronLocalshortcut from "electron-localshortcut";
-import { app, ipcMain, BrowserWindow, globalShortcut, Tray, Menu, nativeImage } from "electron";
+import { app, BrowserWindow, ipcMain, Tray, Menu, nativeImage, globalShortcut } from "electron";
 
-dotenv.config();
-const BASE_URL = 'http://localhost:3000';
+// 웹 모듈
+import axios from "axios";
+import https from "https";
+import dotenv from "dotenv";
 
+// 일렉트론 생성 함수
 let mainWindow;
-let tray;
 function createWindow() {
+
   // 브라우저 창을 생성합니다.
   mainWindow = new BrowserWindow({
     width: 1600,
@@ -22,12 +23,23 @@ function createWindow() {
     webPreferences: { preload: join(dirname(fileURLToPath(import.meta.url)), "preload.js") },
   });
 
+  // 웹 연결 URL
+  dotenv.config();
+  const BASE_URL = 'http://localhost:3000';
   mainWindow.loadURL(BASE_URL);
+}
 
-  electronLocalshortcut.register(mainWindow, "F12", () => { console.log("F12 is pressed"); mainWindow.webContents.toggleDevTools() });
+// 이 메소드는 Electron의 초기화가 완료되고
+// 브라우저 윈도우가 생성될 준비가 되었을때 호출된다.
+app.whenReady().then(createWindow).then(async () => {
 
+  // 기본 생성 세팅
+  app.on("window-all-closed", () => { if (process.platform !== "darwin") { app.quit() } });
+  app.on("activate", () => { if (BrowserWindow.getAllWindows().length === 0) { createWindow() } });
+
+  // 타이틀 바 옵션
+  ipcMain.on("hidden", (event) => { mainWindow.hide() });
   ipcMain.on("minimize", (event) => { mainWindow.minimize() });
-
   ipcMain.on("maximize", (event) => {
     if (mainWindow.isMaximized()) {
       mainWindow.restore();
@@ -38,38 +50,18 @@ function createWindow() {
     }
   });
 
-  ipcMain.on("hidden", (event) => { mainWindow.hide() });
+  // 트레이 세팅
+  const tray = new Tray(nativeImage.createFromPath(join(dirname(fileURLToPath(import.meta.url)), "../../public/icon.png")));
+  tray.setToolTip("react-electron-boilerplate");
+  tray.on("double-click", () => { mainWindow.show() });
+  tray.setContextMenu(Menu.buildFromTemplate([ { label: "켜기", type: "normal", click: () => mainWindow.show() }, { label: "끄기", type: "normal", click: () => app.quit() } ]));
 
-  app.on("window-all-closed", () => { if (process.platform !== "darwin") { app.quit() } });
+  // F5 키를 누르면 현재 포커스된 창 새로고침
+  globalShortcut.register('F5', () => { console.log('F5 is pressed'); mainWindow.reload() })
 
-  app.on("activate", () => { if (BrowserWindow.getAllWindows().length === 0) { createWindow() } });
-}
-
-// 이 메소드는 Electron의 초기화가 완료되고
-// 브라우저 윈도우가 생성될 준비가 되었을때 호출된다.
-app.whenReady().then(createWindow).then(async () => {
-    const icon = nativeImage.createFromPath(join(dirname(fileURLToPath(import.meta.url)), "../../public/icon.png"));
-
-    globalShortcut.register('F5', () => {
-      let win = BrowserWindow.getFocusedWindow();
-      if (win) {
-        win.reload();
-      }
-    });
-
-    tray = new Tray(icon);
-
-    const contextMenu = Menu.buildFromTemplate([
-      { label: "켜기", type: "normal", click: () => mainWindow.show() },
-      { label: "끄기", type: "normal", click: () => app.quit() },
-    ]);
-
-    tray.setToolTip("react-electron-boilerplate");
-    tray.setContextMenu(contextMenu);
-    tray.on("double-click", () => { mainWindow.show() });
-
-    app.on('will-quit', () => {
-      // 애플리케이션이 종료되기 전에 모든 단축키를 해제합니다.
-      globalShortcut.unregisterAll();
-    });
-  });
+  // F12 개발자 도구 열기
+  electronLocalshortcut.register(mainWindow, "F12", () => { console.log("F12 is pressed"); mainWindow.webContents.toggleDevTools() });
+  
+  // 애플리케이션이 종료되기 전에 단축키 해제
+  app.on('will-quit', () => { globalShortcut.unregisterAll() });
+});
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import "@/globals.css";
 //////////////// electron components ////////////////
-const electron = window.electron;
 import TitleBar from "@/electron/TitleBar";
 /////////////////////////////////////////////////////
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
-    {typeof electron !== "undefined" && <TitleBar />}
+    {typeof window.electron !== "undefined" && <TitleBar />}
     <App />
   </>,
 )
