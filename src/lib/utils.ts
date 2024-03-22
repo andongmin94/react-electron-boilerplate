19cb300a27a3d44297bb38d2a29e12b56759493b
김상훈 <k1016h@naver.com>
2024-03-22 21:45:21 +0900

    init page router

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+interface ElectronAPI {
+  send: (channel: string, data?: any) => void;
+  on: (channel: string, func: (event: any, ...args: any[]) => void) => void;
+  removeAllListeners: (channel: string) => void;
+}
+
+declare global {
+  interface Window {
+    electron: ElectronAPI;
+  }
+}
