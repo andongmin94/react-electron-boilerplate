568be3260ef7aeabd1abf8eac1f6ee92c694d570
안동민 <andongmin94@gmail.com>
2024-05-03 13:29:23 +0900

    Modify config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-};
+  output: 'export',
+  // Optional: Add a railing slash to all paths '/about' -> '/about/'
+  // trailingSlash: true,
+  // Optional: Change the output directory 'out' -> 'dist'
+  // distDir: 'dist',
+  images: {
+    unoptimized: true
+  }
+}
 
 export default nextConfig;
